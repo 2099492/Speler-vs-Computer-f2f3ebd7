54668fe71164f1b17deddd3960db5654c68dd9c5
2099492 <2099492@talnet.com>
2019-12-18 15:30:54 +0100

    Uitwerking van de exercise

---
+++ computer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"> 
+	<title>Steen, Papier, Schaar</title>
+</head>
+<body>
+        <h1>Steentje en Paper Sciscor</h1>
+		<h2>(mindere) Speler 1</h2>
+		<?php
+		echo "<form method='get' action='end2.php'>
+		<select name='drop'>
+		<option>Steen</option>
+		<option>Papier</option>
+		<option>Schaar</option>
+		</select>
+		<input type='submit' value='submit' name='submit'></form>";
+			if (isset($_GET["submit"])) {
+				$p1c = $_GET["drop"];
+			}
+		?>
+		
+</body>
+</html>
